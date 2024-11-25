38f2aba44995310c846488bdd93ff15fec2558e0
Terence Toledo <tmtoledo@kpmg.com>
2024-11-26 06:26:49 +0800

    Initial commit

---
+++ src/webparts/resReservation/components/IResReservationState.ts
@@ -0,0 +1,34 @@
+export interface IResReservationState {
+    departmentList : any;
+    buildingList: any;
+    departmentSectorMap: any;
+    venueList: any;
+    showCSRDField: boolean;
+    layoutList: any;
+    venueImage: any;
+    toggler: boolean;
+    facilitiesAvailable:any;
+    capacityperLayout: any;
+    princialList:any;
+    participantList: any;
+    purposeOfUseList:any;
+    showFacilityDialog: boolean;
+    facilityList: any;
+    quantityList: any;
+    facilityData: any;
+    saveDialog: boolean;
+    files: any;
+    crsdMemberList: any;
+    requestorEmail: any;
+    isSavingDone: boolean;
+    isSavingFailure: boolean;
+    isFssManaged: boolean;
+    saveStart: boolean;
+    venueId: any;
+    failureMessage: any;
+    selectedID: any;
+    ddMemeberList: any;
+    isddMember: boolean;
+    fssMemberList: any;
+    }
+  
